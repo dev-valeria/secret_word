c264b1cf8686097fcad96d2c48b263077926be8c
Valeria <valleriademelo@gmail.com>
2024-08-10 22:36:22 -0300

    feat: create project components

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+//CSS
 import './App.css';
 
+//React 
+import { useCallback, useEffect, useState } from 'react';
+
+//Components
+import StartScreen from './components/StartScreen';
+
+//data
+import { wordsList } from './data/words';
+import GameOver from './components/GameOver';
+import Game from './components/Game'
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+]
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
   return (
     <div className="App">
-      <h2>Secret word</h2>
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   );
 }
