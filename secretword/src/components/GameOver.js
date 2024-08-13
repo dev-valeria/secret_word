450b44c2b40eed10b35e9f5bf8411f85f0120f3f
Valeria <valleriademelo@gmail.com>
2024-08-12 23:05:04 -0300

    feat: upgrade CSS styles for improved design and responsiveness

--- secretword/src/components/GameOver.js
+++ secretword/src/components/GameOver.js
@@ -3,7 +3,7 @@ import './GameOver.css'
 
 const GameOver = ({ retry, score }) => {
   return (
-    <div>
+    <div className='gameOver'>
       <h1>Fim do jogo!</h1>
       <h2>A sua pontuação foi: <span>{score}</span></h2>
       <button onClick={retry}>Resetar jogo</button>
